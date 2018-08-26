efba221a786074235ee2c6ec0b955ab1e43de09d
John Kiragu <mutuakiragu@gmail.com>
2018-08-26 22:31:57 +0300

    Implement some cli parsers

--- to-do/README.md
+++ to-do/README.md
@@ -1,1 +1,2 @@
 # to-do
+Simple todo app to manage my schedule on the terminal
--- to-do/app/Main.hs
+++ to-do/app/Main.hs
@@ -1,6 +1,43 @@
-module Main where
+module Main (main) where
 
-import Lib
+import Options.Applicative
 
-main :: IO ()
-main = someFunc
+type ItemIndex = Int
+
+-- This makes more sense as it is a maybe of type String
+-- Could be something we may have it or not
+-- Crafting what an interpretere could look like is not something
+-- I want to do
+type ItemDescription = Maybe String
+
+data Options = Options FilePath ItemIndex ItemDescription deriving Show
+
+defaultDataPath:: FilePath
+defaultDataPath = "~/.to-do.yaml"
+
+optionsParser :: Parser Options
+optionsParser = Options
+    <$>dataPathParser
+    <*>itemIndexParser
+    <*>itemDescriptionParser
+
+dataPathParser:: Parser FilePath
+dataPathParser = strOption $
+    value defaultDataPath
+    <> long "data-path"
+    <> short 'p'
+    <> metavar "DATAPATH"
+    <> help ("path to data file(default " ++defaultDataPath++ ")")
+
+itemIndexParser:: Parser ItemIndex
+itemIndexParser = argument auto(metavar "ITEMINDEX" <> help "index of item")
+
+itemDescriptionParser:: Parser ItemDescription
+itemDescriptionParser =
+    Just <$> itemDescriptionValueParser
+    <|> flag' Nothing (long "clear-desc") -- What happens when we pass a clear desc function here
+
+main::IO()
+main = do
+      options <- execParser(info (optionsParser) (progDesc "To-Do list manager"))
+      putStrLn $ "options = "++ show options
