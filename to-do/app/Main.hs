34b5776afad26d4f4cb5092d68ed1e0d6f31877c
John Kiragu <mutuakiragu@gmail.com>
2018-08-27 10:43:18 +0300

    Remove redundant comments

--- to-do/app/Main.hs
+++ to-do/app/Main.hs
@@ -4,10 +4,6 @@ import Options.Applicative
 
 type ItemIndex = Int
 
--- This makes more sense as it is a maybe of type String
--- Could be something we may have it or not
--- Crafting what an interpretere could look like is not something
--- I want to do
 type ItemDescription = Maybe String
 
 data Options = Options FilePath ItemIndex ItemDescription deriving Show
@@ -35,7 +31,7 @@ itemIndexParser = argument auto(metavar "ITEMINDEX" <> help "index of item")
 itemDescriptionParser:: Parser ItemDescription
 itemDescriptionParser =
     Just <$> itemDescriptionValueParser
-    <|> flag' Nothing (long "clear-desc") -- What happens when we pass a clear desc function here
+    <|> flag' Nothing (long "clear-desc")
 
 main::IO()
 main = do
